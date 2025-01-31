1a0ba4771044cc260e2af529f6c19d9d0baac0d6
Gladwin <GMAPHUTI6@GMAIL.COM>
2025-01-31 15:09:51 +0200

    Delete HTML LogIn directory

--- HTML LogIn/lowercase.js
+++
@@ -1,6 +0,0 @@
-    document.getElementById('username').addEventListener('input', function () {
-        this.value = this.value.toLowerCase(); // Convert input to lowercase
-    });
-    //document.getElementById('username').addEventListener('input', function () {
-    //    this.value = this.value.toLowerCase(); // Convert input to lowercase
-    //});
